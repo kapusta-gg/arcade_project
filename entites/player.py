aba7b55e0d5a25cb3e9deea3dd52db1b96bb2552
bogda <bogdashka04@gmail.com>
2025-11-18 19:02:13 +0300

    Инициализация

---
+++ entites/player.py
@@ -0,0 +1,32 @@
+import arcade
+from consts import *
+
+from PIL import Image
+
+
+class Player(arcade.Sprite):
+    IMAGE = Image.open(
+        "source/PNG/Players/Tiles/tile_0001.png").convert("RGBA")
+    SPEED = 20
+
+    def __init__(self):
+        super().__init__(arcade.Texture(self.IMAGE), 1,
+                         WINDOW_SIZES[0] // 2, WINDOW_SIZES[1] // 2)
+        self._setup()
+
+    def _setup(self):
+        self.velocity = 0, 0
+
+    def update(self, delta_time, keys: set):
+        self.velocity = Player._update_velocity(keys)
+        self.center_x = self.center_x + self.change_x * delta_time
+        self.center_y = self.center_y + self.change_y * delta_time
+
+    @classmethod
+    def _update_velocity(cls, keys: set) -> tuple[int, int]:
+        x_vel = y_vel = 0
+        x_vel -= cls.SPEED if 97 in keys else 0
+        x_vel += cls.SPEED if 100 in keys else 0
+        y_vel -= cls.SPEED if 115 in keys else 0
+        y_vel += cls.SPEED if 119 in keys else 0
+        return x_vel, y_vel
---
+++ main.py
@@ -0,0 +1,34 @@
+import arcade
+from entites.player import Player
+from consts import *
+
+
+class MainWindow(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, resizable=False)
+        self.sprites = arcade.sprite_list.SpriteList()
+        self.keys = set()
+
+    def setup(self):
+        player = Player()
+        self.sprites.append(player)
+
+    def on_draw(self):
+        self.clear()
+        self.sprites.draw()
+
+    def on_update(self, delta_time):
+        self.sprites.update(delta_time, self.keys)
+
+    def on_key_press(self, symbol, modifiers):
+        self.keys.add(symbol)
+
+    def on_key_release(self, symbol, modifiers):
+        self.keys.remove(symbol)
+
+
+if __name__ == "__main__":
+    print(arcade.color.AERO_BLUE)
+    window = MainWindow(*WINDOW_SIZES, "Main")
+    window.setup()
+    arcade.run()
