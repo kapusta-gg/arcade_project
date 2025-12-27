521015611abfa522e7a7b79d998e6e2f2cd82ce7
bogda <bogdashka04@gmail.com>
2025-12-27 23:37:29 +0300

    Добавлена камера и уровень

--- consts.py
+++ consts.py
@@ -15,3 +15,6 @@ with open("settings.json", "r") as f:
 PIXELS_BOARD = 10
 X, Y = 0, 1
 WINDOWS_OPTIONS = ["1280x1024", "1920x1080", "2000x1080"]
+CAMERA_LERP = 0.12
+CAMERA_WIDTH = 16 * 80
+CAMERA_HEIGHT = 9 * 80
--- views/game_view.py
+++ views/game_view.py
@@ -10,20 +10,35 @@ class GameView(arcade.View):
         self.sprites = arcade.SpriteList()
         self.enemis = arcade.SpriteList()
         self.keys = set()
+        self.camera = arcade.Camera2D()
+        self.camera.width = CAMERA_WIDTH
+        self.camera.height = CAMERA_HEIGHT
 
     def setup(self):
         arcade.set_background_color(arcade.color.FERN_GREEN)
         self.player = Player()
         self.sprites.append(self.player)
 
+        self.map = arcade.load_tilemap("./maps/level1.tmx", scaling=2.5)
+        self.scene = arcade.Scene.from_tilemap(self.map)
+
         self._create_enemies(1)
 
     def on_draw(self):
         self.clear()
+        self.scene.draw()
         self.sprites.draw()
+        self.camera.use()
 
     def on_update(self, delta_time):
         self.sprites.update(delta_time, self.keys)
+
+        pos = (self.player.center_x, self.player.center_y)
+        self.camera.position = arcade.math.lerp_2d(
+            self.camera.position,
+            pos,
+            CAMERA_LERP,
+        )
 
     def on_key_press(self, symbol, modifiers):
         self.keys.add(symbol)
