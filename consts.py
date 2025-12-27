4d6fae1e35b9d1e89c6703d6e5205373e8579e71
bogda <bogdashka04@gmail.com>
2025-12-27 13:57:10 +0300

    настройки игры перенесены в json-файлы

--- consts.py
+++ consts.py
@@ -1,3 +1,8 @@
-WINDOW_SIZES = 1280, 1024
+import json
+
+with open("settings.json", "r") as f:
+    data = json.load(f)["settings"]
+    window_size = data["window_sizes"]
+WINDOW_SIZES = window_size
 X, Y = 0, 1
 WINDOWS_OPTIONS = ["1280x1024", "1920x1080", "2000x1080"]
